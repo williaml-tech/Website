84bea3d74738875fd479c4108731e9842729b4b8
williaml-tech <william.l@drblinds.com.au>
2026-01-20 09:29:42 +1100

    Update next.config.ts
    重定向

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/appointment',
+        destination: 'https://www.fresha.com/book-now/klinik-ka-aesthetic-clinic-fivxhb80/all-offer?share=true&pId=2723491',
+        permanent: false, // 暂时设为 false，确定没问题后再改为 true
+      },
+    ]
+  },
 };
 
 export default nextConfig;
